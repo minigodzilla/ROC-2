1273404f0d0972bbf4810a9d2854b2b4363bf416
Steve Diabo <stevediabo@gmail.com>
2021-02-03 17:09:25 -0500

    initial (wow)

---
+++ assets/scripts/script.js
@@ -0,0 +1,279 @@
+//----------------------------------------------------------------------------//
+// Register Form                                                              //
+//----------------------------------------------------------------------------//
+
+$(function()
+{
+
+	$('.dv-form-toggle-option').on('click', function() {
+
+		var value = $(this).attr('data-value');
+
+		$('.dv-form-toggle-input').attr('value', value);
+
+	})
+
+
+	////////////////////////////////////////////////////////////////////////////////
+
+	var $form              = $('#dv-register-form');
+	var $inputs            = $form.find ('.form-control');
+	var $email             = $form.find ('.form-control[name=Email]');
+	var $button            = $form.find ('.dv-btn-submit');
+	var errorState         = false;
+
+	////////////////////////////////////////////////////////////////////////////////
+
+	function isEmail(email) {
+
+		var regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+		if(!regex.test(email))
+		{
+			return false;
+		}
+
+		else
+		{
+			return true;
+		}
+	}
+
+	$inputs.blur (function()
+	{
+		if (!$(this).val())
+		{
+			$(this).removeClass ('is-valid').addClass ('is-invalid');
+		}
+
+		else
+		{
+			$(this).removeClass ('is-invalid').addClass ('is-valid');
+		}
+	});
+
+	$email.blur (function()
+	{
+		if (!isEmail ($(this).val()))
+		{
+			$(this).removeClass ('is-valid').addClass ('is-invalid');
+		}
+
+		else
+		{
+			$(this).removeClass ('is-invalid').addClass ('is-valid');
+		}
+
+	});
+
+	$form.submit (function(e)
+	{
+		// prevent default submit behaviour
+		e.preventDefault();
+
+		// reset error state
+		errorState = false;
+
+		// check for empty fields
+		$inputs.each (function()
+		{
+			if (!$(this).val())
+			{
+				$(this).removeClass ('is-valid').addClass ('is-invalid');
+				errorState = true;
+			}
+
+			else
+			{
+				$(this).removeClass ('is-invalid').addClass ('is-valid');
+			}
+		});
+
+		// check whether email is valid
+		if (!isEmail ($email.val()))
+		{
+			$email.removeClass ('is-valid').addClass ('is-invalid');
+			errorState = true;
+		}
+
+		else
+		{
+			$email.removeClass ('is-invalid').addClass ('is-valid');
+		}
+
+		// if form has errors
+		if (errorState)
+			return false;
+
+		// now we do ajax
+		// get form
+		var form = $('#dv-register-form')[0];
+
+		// create an FormData object 
+		var data = new FormData(form);
+
+		// prevent duplicate submissions
+		$form.find('.dv-btn-submit').prop('disabled', true);
+
+		// do a barrel roll
+
+		//gtag('event', 'conversion', {'send_to': 'AW-755396735/ylHBCKzzuZ0BEP_gmegC'});
+		gtag('event', 'Signup', {'event_category': "Conversions", 'event_label': "CompleteRegistration", 'value': "0"});
+		fbq('track', 'CompleteRegistration');
+
+		$.ajax({
+			type: "POST",
+			enctype: 'multipart/form-data',
+			url: "/register.php",
+			data: data,
+			processData: false,
+			contentType: false,
+			cache: false,
+			timeout: 800000,
+			success: function (data) {
+
+				$form.addClass('dv-success');
+
+			},
+			error: function (e) {
+
+				console.log("ERROR : ", e);
+
+				$form.addClass('dv-error');
+
+			}
+		});
+
+	});
+
+});
+
+
+//----------------------------------------------------------------------------//
+// Footer Mailing List Form                                                   //
+//----------------------------------------------------------------------------//
+
+$(function()
+{
+
+	////////////////////////////////////////////////////////////////////////////////
+
+	var $form              = $('#dv-footer-form');
+	var $email             = $form.find ('.form-control[name=Email]');
+	var $checkbox          = $form.find ('.form-check-input[name=consent]');
+	var $button            = $form.find ('.dv-btn-submit');
+	var errorState         = false;
+
+	////////////////////////////////////////////////////////////////////////////////
+
+	function isEmail(email) {
+
+		var regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+		if(!regex.test(email))
+		{
+			return false;
+		}
+
+		else
+		{
+			return true;
+		}
+	}
+
+	$form.submit (function(e)
+	{
+		// prevent default submit behaviour
+		e.preventDefault();
+
+		// reset error state
+		errorState = false;
+
+		// check whether email is valid
+		if (!isEmail ($email.val()))
+		{
+			$email.removeClass ('is-valid').addClass ('is-invalid');
+			errorState = true;
+		}
+
+		else
+		{
+			$email.removeClass ('is-invalid').addClass ('is-valid');
+		}
+
+		// check whether consent checkbox is checked
+		if ($checkbox.prop('checked')!=true)
+		{
+			$checkbox.removeClass ('is-valid').addClass ('is-invalid');
+			errorState = true;
+		}
+		else
+		{
+			$checkbox.removeClass ('is-invalid');
+		}
+
+		// if form has errors
+		if (errorState)
+			return false;
+
+		// now we do ajax
+		// get form
+		var form = $('#dv-footer-form')[0];
+
+		// create an FormData object 
+		var data = new FormData(form);
+
+		// prevent duplicate submissions
+		$form.find('.dv-btn-submit').prop('disabled', true);
+
+		// do a barrel roll
+		$.ajax({
+			type: "POST",
+			enctype: 'multipart/form-data',
+			url: "/register.php",
+			data: data,
+			processData: false,
+			contentType: false,
+			cache: false,
+			timeout: 800000,
+			success: function (data) {
+
+				$form.addClass('dv-success');
+
+			},
+			error: function (e) {
+
+				console.log("ERROR : ", e);
+
+				$form.addClass('dv-error');
+
+			}
+		});
+
+	});
+
+});
+
+//----------------------------------------------------------------------------//
+// ScrollMagic Animation                                                      //
+//----------------------------------------------------------------------------//
+
+$(function()
+{
+
+	// Create an instance of the Scroll Magic Controller
+	let controller = new ScrollMagic.Controller();
+
+	$('.dv-animated-section').each(function () {
+
+		var $this = $(this);
+
+		new ScrollMagic.Scene
+		({
+			triggerElement: this, triggerHook: 0.5, reverse: false
+		})
+		.setClassToggle(this, 'dv-animated')
+		.addTo(controller);
+	});
+
+});
--- gulpfile.js
+++ gulpfile.js
@@ -2,35 +2,21 @@ const gulp = require('gulp');
 const gulpIf = require('gulp-if');
 const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
-const htmlmin = require('gulp-htmlmin');
 const cssmin = require('gulp-cssmin');
 const uglify = require('gulp-uglify');
-const imagemin = require('gulp-imagemin');
 const concat = require('gulp-concat');
 const jsImport = require('gulp-js-import');
 const sourcemaps = require('gulp-sourcemaps');
-const htmlPartial = require('gulp-html-partial');
 const cssbeautify = require('gulp-cssbeautify');
-const htmlbeautify = require('gulp-html-beautify');
 const isProd = process.env.NODE_ENV === 'prod';
 
 var options = { };
 
-const htmlFile = [
-    'src/**/*.html'
-]
-
 function html() {
-    return gulp.src(htmlFile)
-        .pipe(htmlPartial({
-            basePath: 'src/assets/partials/'
-        }))
-        .pipe(htmlbeautify())
-        .pipe(gulp.dest('public'));
 }
 
 function css() {
-    return gulp.src('src/assets/sass/style.scss')
+    return gulp.src('assets/sass/style.scss')
         .pipe(sourcemaps.init())
         .pipe(sass({
             includePaths: ['node_modules']
@@ -42,7 +28,7 @@ function css() {
         }))
         .pipe(sourcemaps.write())
         .pipe(gulpIf(isProd, cssmin()))
-        .pipe(gulp.dest('public/assets/css/'));
+        .pipe(gulp.dest('assets/css/'));
 }
 
 function js() {
@@ -50,24 +36,19 @@ function js() {
         'node_modules/jquery/dist/jquery.min.js',
         'node_modules/bootstrap/dist/js/bootstrap.min.js',
         'node_modules/scrollmagic/scrollmagic/minified/ScrollMagic.min.js',
-        'node_modules/rellax/rellax.min.js',
-        'node_modules/owl.carousel/dist/owl.carousel.min.js',
-        'src/assets/scripts/script.js'])
+        'assets/scripts/script.js'])
         .pipe(sourcemaps.init())
         .pipe(jsImport({
             hideConsole: true
         }))
         .pipe(concat('script.js'))
+        .pipe(gulpIf(isProd, uglify()))
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('public/assets/js'));
+        .pipe(gulp.dest('assets/js'));
 }
 
 function img() {
-    return gulp.src('src/assets/img/*')
-        //.pipe(imagemin())
-        .pipe(gulp.dest('public/assets/img/'));
 }
-
 
 function fonts() {
 }
@@ -76,7 +57,7 @@ function serve() {
     browserSync.init({
         open: true,
         notify: false,
-        server: './public'
+        server: '.'
     });
 }
 
@@ -87,26 +68,18 @@ function browserSyncReload(done) {
 
 
 function watchFiles() {
-    gulp.watch('src/**/*.html', gulp.series(html, browserSyncReload));
-    gulp.watch('src/assets/**/*.scss', gulp.series(css, browserSyncReload));
-    gulp.watch('src/assets/**/*.js', gulp.series(js, browserSyncReload));
-    gulp.watch('src/assets/img/**/*.*', gulp.series(img));
+    gulp.watch('*.html', gulp.series(browserSyncReload));
+    gulp.watch('assets/sass/*.scss', gulp.series(css, browserSyncReload));
+    gulp.watch('assets/scripts/*.js', gulp.series(js, browserSyncReload));
 
     return;
 }
 
-function del() {
-    return gulp.src('public/*', {read: false})
-        .pipe(clean());
-}
-
 exports.css = css;
-exports.html = html;
 exports.js = js;
-exports.del = del;
 
 // this is the serve 
-exports.default = gulp.parallel(html, css, js, img, watchFiles, serve);
+exports.default = gulp.parallel(css, js, watchFiles, serve);
 
 // this is the build task
-exports.build = gulp.series(del, html, css, js, img);
+exports.build = gulp.series(css, js);
